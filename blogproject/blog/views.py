01f1a941b2a58269bc4f07d3d87bdd64f25e8ecc
joseunghyo <seunghyo7742@naver.com>
2021-05-17 22:10:20 +0900

    plz

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -4,7 +4,7 @@ from .models import Blog
 
 def home(request):
     blogs = Blog.objects.all()
-    return render(request, 'home.html', {"blogs":blogs})
+    return render(request, 'home.html', {'blogs':blogs})
 
 def detail1(request, id):
     blog = get_object_or_404(Blog, pk = id)
